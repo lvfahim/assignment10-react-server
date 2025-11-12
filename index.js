4e44de55725f7bb1aa3087f132bdb29ae4b5024d
lvfahim <mdfahim26320@gmail.com>
2025-11-12 23:35:17 +0600

    my myBookingList is ok

--- index.js
+++ index.js
@@ -23,48 +23,70 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        const motorio=client.db('Motorio');
-        const carDataCollection=motorio.collection('carData')
-        const carListingCollection=motorio.collection('carListing')
+        const motorio = client.db('Motorio');
+        const carDataCollection = motorio.collection('carData')
+        const carBookingCollection = motorio.collection('carBooking')
         // Send a ping to confirm a successful connection
-        app.get('/carData', async (req,res)=>{
-            const cursor= carDataCollection.find()
+        // car Booking 
+        app.post('/carBooking', async (req, res) => {
+            const newBooking = req.body
+            const result = await carBookingCollection.insertOne(newBooking)
+            res.send(result)
+        })
+        app.get('/myBookingList', async (req, res) => {
+            const email = req.query.email
+            const quary = {}
+            if (email) {
+                quary.email = email
+            }
+            const cursor = carBookingCollection.find(quary).sort({ price: -1 })
             const result = await cursor.toArray()
             res.send(result)
         })
-        app.get('/carDetails', async(req,res)=>{
-            const project={category:1,price:1,name:1,providerName:1,image:1}
-            const sort = {price:-1}
+        app.get('/myBook',async(req,res)=>{
+            const cursor = carBookingCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        // Car Data 
+        app.get('/carData', async (req, res) => {
+            const cursor = carDataCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/carDetails', async (req, res) => {
+            const project = { category: 1, price: 1, name: 1, providerName: 1, image: 1 }
+            const sort = { price: -1 }
             const cursor = carDataCollection.find().sort(sort).limit(8).project(project)
             const result = await cursor.toArray()
             res.send(result)
-            
+
         })
-        app.get('/carDetails/:id', async (req,res)=>{
-            const id=req.params.id;
-            const quary={_id: new ObjectId(id)}
+        app.get('/carDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: new ObjectId(id) }
             const result = await carDataCollection.findOne(quary)
             res.send(result)
         })
-        app.get('/carDetailsForall', async(req,res)=>{
-            const project={category:1,price:1,name:1,providerName:1,image:1}
-            const sort = {price:-1}
+        app.get('/carDetailsForall', async (req, res) => {
+            const project = { category: 1, price: 1, name: 1, providerName: 1, image: 1 }
+            const sort = { price: -1 }
             const cursor = carDataCollection.find().project(project).sort(sort)
             const result = await cursor.toArray()
             res.send(result)
-            
+
         })
-          app.get('/myCarList', async(req,res)=>{
-            const email=req.query.email
+        app.get('/myCarList', async (req, res) => {
+            const email = req.query.email
             const quary = {}
-            if(email){
-                quary.providerEmail=email
+            if (email) {
+                quary.providerEmail = email
             }
-            const cursor = carDataCollection.find(quary)
-            const result =await cursor.toArray()
+            const cursor = carDataCollection.find(quary).sort({ price: -1 })
+            const result = await cursor.toArray()
             res.send(result)
         })
-        app.post('/carData', async (req,res)=>{
+        app.post('/carData', async (req, res) => {
             const newCarData = req.body
             const result = await carDataCollection.insertOne(newCarData);
             res.send(result)
